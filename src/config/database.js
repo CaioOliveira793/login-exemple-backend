c36b76c1029f0522d9b679c384356c34bd559f60
Caio Oliveira <caio.vsoliveira23@gmail.com>
2020-02-23 14:20:40 -0300

    refactor: add debug configs to db connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
 	dialect: 'postgres',
-	host: process.env.PG_HOST,
-	username: process.env.PG_USER,
-	password: process.env.PG_PASSWORD,
-	database: process.env.PG_DATABASE,
+	host: process.env.PG_HOST || 'localhost',
+	username: process.env.PG_USER || 'node_user',
+	password: process.env.PG_PASSWORD || 'node_password',
+	database: process.env.PG_DATABASE || 'login_exemple',
 	define: {
 		timestamps: true,
 		underscored: true
