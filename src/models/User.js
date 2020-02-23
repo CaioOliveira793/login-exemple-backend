1b75f23d3fc3da6824ac6b70863d2e54c6720295
Caio Oliveira <caio.vsoliveira23@gmail.com>
2020-02-23 14:23:58 -0300

    refactor: add adjustments to User model

--- src/models/User.js
+++ src/models/User.js
@@ -3,10 +3,23 @@ const { Model, DataTypes } = require('sequelize');
 class User extends Model {
 	static init(sequelize) {
 		super.init({
-			firstName: DataTypes.STRING,
-			lastName: DataTypes.STRING,
-			email: DataTypes.STRING,
-			password: DataTypes.STRING
+			firstName: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			lastName: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			email: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				unique: true
+			},
+			password: {
+				type: DataTypes.STRING,
+				allowNull: false
+			}
 		}, {
 			sequelize,
 			tableName: 'users',
