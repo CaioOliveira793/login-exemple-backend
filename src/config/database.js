2693dfd0af8c5a52f1f4e0160bc079b4592fde39
Caio Oliveira <caio.vsoliveira23@gmail.com>
2020-04-13 16:43:27 -0300

    chore: change development database to sqlite

--- src/config/database.js
+++ src/config/database.js
@@ -2,24 +2,25 @@ require('./variables.env');
 
 module.exports = {
 	development: {
-		dialect: 'postgres',
-		host: process.env.PG_HOST || 'localhost',
-		port: process.env.PG_PORT || 5432,
-		username: process.env.PG_USER || 'node_user',
-		password: process.env.PG_PASSWORD || 'node_password',
-		database: process.env.PG_DATABASE || 'login_exemple',
+		dialect: process.env.DB_DIALECT || 'sqlite',
+		storage: process.env.DB_STORAGE || 'src/database/development.sqlite',
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DATABASE || 'login_exemple',
 		define: {
 			timestamps: true,
 			underscored: true
 		}
 	},
 	production: {
-		dialect: 'postgres',
-		host: process.env.PG_HOST,
-		port: process.env.PG_PORT,
-		username: process.env.PG_USER,
-		password: process.env.PG_PASSWORD,
-		database: process.env.PG_DATABASE,
+		dialect: process.env.DB_DIALECT,
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DATABASE,
 		define: {
 			timestamps: true,
 			underscored: true
